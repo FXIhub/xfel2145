f1c7b2af0bf715f46288420b5a5e3f5c5bc6e48e
Benedikt Daurer <benedikt.daurer@icm.uu.se>
2019-05-16 12:01:21 +0200

    Added conf.py that seems to work with karabo-bridge-replay and latest hummingbird in xfel2145 branch

---
+++ conf.py
@@ -0,0 +1,31 @@
+"""
+For testing the EuXFEL backend, start the karabo replay:
+
+./karabo-bridge-replay '0.0.0.0:1234' `/gpfs/exfel/u/scratch/XMPL/201750/p700000/stream_records/SPB_DET_AGIPD1M-1_CAL_APPEND_CORR-2019-02-21T11.04.11Z
+
+from the karabo-bridge (https://github.com/European-XFEL/karabo-bridge-py).
+and then start the Hummingbird backend:
+
+./hummingbird.py -b conf.py
+"""
+import plotting.image
+import analysis.agipd
+import analysis.event
+from backend import add_record
+
+state = {}
+state['Facility'] = 'EuXFEL'
+state['EuXFEL/DataSource'] = 'tcp://0.0.0.0:1234'
+state['EuXFEL/DataFormat'] = 'Calib'
+
+event_number = 0
+def onEvent(evt):
+    global event_number
+    #analysis.event.printKeys(evt)
+    #analysis.event.printNativeKeys(evt)
+    analysis.event.printProcessingRate()
+    T = evt["eventID"]["Timestamp"]
+    #print(event_number, T.timestamp, T.pulseId, T.cellId, T.trainId)
+    agipd_0 = add_record(evt['analysis'], 'analysis', 'AGIPD', evt['photonPixelDetectors']['AGIPD'].data[:,:,0])
+    plotting.image.plotImage(agipd_0)
+    event_number += 1
